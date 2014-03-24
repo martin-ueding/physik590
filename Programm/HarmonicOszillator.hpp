fc0d8999a934321f5b0ef74772eb0c5598c6a30f
Martin Ueding <dev@martin-ueding.de>
2014-03-24 21:43:53 +0100

    Add docstring

--- Programm/HarmonicOszillator.hpp
+++ Programm/HarmonicOszillator.hpp
@@ -1,6 +1,10 @@
 // Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
 // Licensed under The GNU Public License Version 2 (or later)
 
+/**
+  @file
+  */
+
 #ifndef HARMONICOSZILLATOR_H
 #define HARMONICOSZILLATOR_H
 
