c2063684c446fc564c2d2ae59f4a4778821e46c4
Martin Ueding <dev@martin-ueding.de>
2014-03-25 21:41:26 +0100

    Implement sweep script

--- Programm/sweep.fish
+++ Programm/sweep.fish
@@ -2,4 +2,18 @@
 # Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
 # Licensed under The GNU Public License Version 2 (or later)
 
-
+for mass in 0.2 1.0 4.0
+    for mu in 0.2 1.0 4.0
+        for a in 0.02 0.1 0.4
+            for rounds in 5 10
+                set dir "$mass_$mu_$a_$rounds"
+                echo $dir
+                mkdir $dir
+                pushd $dir
+                ../metropolis --mass $mass --mu-squared $mu --time-step $a --rounds $rounds
+                ../plotall.py
+                popd
+            end
+        end
+    end
+end
