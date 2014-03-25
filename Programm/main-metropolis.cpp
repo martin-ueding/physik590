b37fc26ef515c29bf9bc7bc8ee459a998030cb81
Martin Ueding <dev@martin-ueding.de>
2014-03-25 17:01:00 +0100

    Group program options in the help

--- Programm/main-metropolis.cpp
+++ Programm/main-metropolis.cpp
@@ -24,29 +24,40 @@ int main(int argc, char **argv) {
     int rounds;
 
 
-    boost::program_options::options_description description("");
-    description.add_options()
+    boost::program_options::options_description options("Program options");
+    options.add_options()
     ("help,h", "Print usage and exit")
-
     ("time-bins,t",  boost::program_options::value<int>(&time_bins)->default_value(100), "Number of bins in the time lattice")
-    ("irw",  boost::program_options::value<double>(&initial_random_width)->default_value(40), "Initial random width")
+    ;
 
+    boost::program_options::options_description init_options("Initialization Options");
+    init_options.add_options()
+    ("irw",  boost::program_options::value<double>(&initial_random_width)->default_value(40), "Initial random width")
     ("margin,m",  boost::program_options::value<double>(&margin)->default_value(40), "Random margin, Δ")
     ("pre-iterations",  boost::program_options::value<int>(&pre_iterations)->default_value(1000), "Iterations to relax the system")
     ("pre-rounds",  boost::program_options::value<int>(&pre_rounds)->default_value(5), "Rounds for a single x_j during relaxation")
+    ;
+    options.add(init_options);
 
+    boost::program_options::options_description iter_options("Iteration Options");
+    iter_options.add_options()
     ("iterations,i",  boost::program_options::value<int>(&iterations)->default_value(10000), "Iterations for the histogram")
     ("rounds,r",  boost::program_options::value<int>(&rounds)->default_value(5), "Rounds for a single x_j")
+    ;
+    options.add(iter_options);
 
+    boost::program_options::options_description hist_options("Histogram Options");
+    hist_options.add_options()
     ("hist-bins,b",  boost::program_options::value<int>(&hist_bins)->default_value(100), "Number of bins in the histogram")
     ;
+    options.add(hist_options);
 
     boost::program_options::variables_map vm;
-    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, description), vm);
+    boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), vm);
     boost::program_options::notify(vm);
 
     if (vm.count("help") > 0) {
-        std::cout << description << std::endl;
+        std::cout << options << std::endl;
         return 0;
     }
 
