f290e422f3db856ef9c7051048cbb7cdddba1ed9
Martin Ueding <dev@martin-ueding.de>
2014-03-25 16:47:58 +0100

    Put all numbers into options

--- Programm/main-metropolis.cpp
+++ Programm/main-metropolis.cpp
@@ -14,12 +14,27 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    int bins;
+    int time_bins;
+    int hist_bins;
+    double initial_random_width;
+    double margin;
+    int iterations;
+    int pre_iterations;
+    int pre_rounds;
+    int rounds;
+
 
     boost::program_options::options_description description("");
     description.add_options()
     ("help,h", "Print usage and exit")
-    ("bins",  boost::program_options::value<int>(&bins)->default_value(100), "Number of bins in the histogram")
+    ("time-bins,t",  boost::program_options::value<int>(&time_bins)->default_value(100), "Number of bins in the time lattice")
+    ("hist-bins,b",  boost::program_options::value<int>(&hist_bins)->default_value(100), "Number of bins in the histogram")
+    ("irw",  boost::program_options::value<double>(&initial_random_width)->default_value(40), "Initial random width")
+    ("margin,m",  boost::program_options::value<double>(&margin)->default_value(40), "Random margin, Δ")
+    ("iterations,i",  boost::program_options::value<int>(&iterations)->default_value(10000), "Iterations for the histogram")
+    ("rounds,r",  boost::program_options::value<int>(&rounds)->default_value(5), "Rounds for a single x_j")
+    ("pre-iterations",  boost::program_options::value<int>(&pre_iterations)->default_value(1000), "Iterations to relax the system")
+    ("pre-rounds",  boost::program_options::value<int>(&pre_rounds)->default_value(5), "Rounds for a single x_j during relaxation")
     ;
 
     boost::program_options::variables_map vm;
@@ -32,29 +47,29 @@ int main(int argc, char **argv) {
     }
 
     HarmonicOszillator ho;
-    Trajectory t(bins, ho);
+    Trajectory t(time_bins, ho);
 
     t.save_plot_file("trajectory-01-init.csv");
     std::cout << "Action after init: " << t.action() << std::endl;
 
-    t.set_to_random(40.);
+    t.set_to_random(initial_random_width);
     t.save_plot_file("trajectory-02-random.csv");
     std::cout << "Action after random: " << t.action() << std::endl;
 
-    t.iteration(1, 40.);
+    t.iteration(pre_rounds, margin);
     t.save_plot_file("trajectory-03-iteration.csv");
     std::cout << "Action after iteration: " << t.action() << std::endl;
 
-    for (int i = 0; i < 1000; i++) {
-        t.iteration(5, 40.);
+    for (int i = 0; i < pre_iterations; i++) {
+        t.iteration(pre_rounds, margin);
     }
     t.save_plot_file("trajectory-04-more_iterations.csv");
     std::cout << "Action after more iterations: " << t.action() << std::endl;
 
-    Histogram histogram(100, 49*1000);
+    Histogram histogram(hist_bins, (time_bins-1)*iterations);
 
-    for (int i = 0; i < 10000; i++) {
-        t.iteration(5, 40.);
+    for (int i = 0; i < iterations; i++) {
+        t.iteration(rounds, margin);
         t.binning_snapshot(histogram);
     }
 
