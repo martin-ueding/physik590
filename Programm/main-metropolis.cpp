43893d6f4782af408f4c7b4050d12ed92fac4b7a
Martin Ueding <dev@martin-ueding.de>
2014-03-25 10:20:47 +0100

    Fix file paths

--- Programm/main-metropolis.cpp
+++ Programm/main-metropolis.cpp
@@ -12,11 +12,11 @@
 
 int main() {
     HarmonicOszillator ho;
-    Trajectory t(10, ho);
+    Trajectory t(50, ho);
 
-    t.save_plot_file("plot-01-init.csv");
+    t.save_plot_file("trajectory-01-init.csv");
     t.set_to_random(40.);
-    t.save_plot_file("plot-02-random.csv");
+    t.save_plot_file("trajectory-02-random.csv");
 
     std::cout << "S: " << t.action() << std::endl;
 
