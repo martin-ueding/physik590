4882699abe263f0cc63ca400ab5b6ccda280e55c
Martin Ueding <dev@martin-ueding.de>
2014-04-02 20:24:28 +0200

    Add GPL header

--- Programm/plotall.py
+++ Programm/plotall.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+# Licensed under The GNU Public License Version 2 (or later)
 
 import argparse
 import matplotlib.pyplot as pl
