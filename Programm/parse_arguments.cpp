31b15123c155244e603325f7b2262d3f898f23ed
Martin Ueding <dev@martin-ueding.de>
2014-04-03 21:08:00 +0200

    C++11 syntax

--- Programm/parse_arguments.cpp
+++ Programm/parse_arguments.cpp
@@ -8,12 +8,12 @@
 #include <iostream>
 
 bool parse_arguments(int argc, char **argv, Settings &settings) {
-    boost::program_options::options_description options("Program options");
+    boost::program_options::options_description options{"Program options"};
     options.add_options()
     ("help,h", "Print usage and exit")
     ;
 
-    boost::program_options::options_description oszillator_options("Oszillator options");
+    boost::program_options::options_description oszillator_options{"Oszillator options"};
     oszillator_options.add_options()
     ("time-bins,t", boost::program_options::value<size_t>(&settings.time_sites)->default_value(1000), "Number of sites in the time lattice")
     ("mass,m", boost::program_options::value<double>(&settings.mass)->default_value(1), "Mass")
@@ -22,7 +22,7 @@ bool parse_arguments(int argc, char **argv, Settings &settings) {
     ;
     options.add(oszillator_options);
 
-    boost::program_options::options_description init_options("Initialization options");
+    boost::program_options::options_description init_options{"Initialization options"};
     init_options.add_options()
     ("irw", boost::program_options::value<double>(&settings.initial_random_width)->default_value(0.63), "Initial random width")
     ("margin", boost::program_options::value<double>(&settings.margin)->default_value(0.632456), "Random margin, Δ")
@@ -31,7 +31,7 @@ bool parse_arguments(int argc, char **argv, Settings &settings) {
     ;
     options.add(init_options);
 
-    boost::program_options::options_description iter_options("Iteration options");
+    boost::program_options::options_description iter_options{"Iteration options"};
     iter_options.add_options()
     ("iterations,i", boost::program_options::value<size_t>(&settings.iterations)->default_value(10000), "Iterations for the histogram")
     ("rounds,r", boost::program_options::value<int>(&settings.rounds)->default_value(5), "Rounds for a single x_j")
@@ -40,7 +40,7 @@ bool parse_arguments(int argc, char **argv, Settings &settings) {
     ;
     options.add(iter_options);
 
-    boost::program_options::options_description hist_options("Histogram options");
+    boost::program_options::options_description hist_options{"Histogram options"};
     hist_options.add_options()
     ("position-hist-bins,b", boost::program_options::value<size_t>(&settings.position_hist_bins)->default_value(1000), "Number of bins in the position histogram")
     ("action-hist-bins", boost::program_options::value<size_t>(&settings.action_hist_bins)->default_value(100), "Number of bins in the action histogram")
