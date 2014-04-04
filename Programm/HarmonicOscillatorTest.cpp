0830a0c0315f84f93a46b1738c74a820de9ee7b3
Martin Ueding <dev@martin-ueding.de>
2014-04-04 20:52:25 +0200

    Do some checks for HarmonicOscillator

--- Programm/HarmonicOscillatorTest.cpp
+++ Programm/HarmonicOscillatorTest.cpp
@@ -4,5 +4,15 @@
 #include "HarmonicOscillatorTest.hpp"
 
 void HarmonicOscillatorTest::test_sign() {
-    CPPUNIT_ASSERT(true);
+    HarmonicOscillator ho{0.1, 1.0, 1.0};
+
+    // Check that the action increases when the trajectory gets a kink.
+    CPPUNIT_ASSERT(ho.action_difference(0.0, 0.0, 1.0, 0.0) > 0);
+
+    // Check that the action decreases when the trajectory is flattened.
+    CPPUNIT_ASSERT(ho.action_difference(0.0, 1.0, 0.0, 0.0) > 0);
+
+    // Check that the action does not change when the trajectory does not
+    // change either.
+    CPPUNIT_ASSERT(ho.action_difference(1.0, 1.0, 1.0, 1.0) == 0);
 }
