b14f94d197c26dd4ead0595bc6fe962631b524d2
Martin Ueding <dev@martin-ueding.de>
2014-04-05 11:00:02 +0200

    Other filenames

--- Programm/Settings.cpp
+++ Programm/Settings.cpp
@@ -10,7 +10,9 @@ std::string Settings::generate_filename(std::string prefix, std::string suffix) 
 
     oss << prefix;
 
-    oss << iterations;
+    oss << "i{" << iterations << "}";
+    oss << separator;
+    oss << "o{" << mu_squared << "}";
 
     oss << suffix;
 
