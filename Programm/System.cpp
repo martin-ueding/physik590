b3d9651765cda24e855cb501875e8e7866201d55
Martin Ueding <dev@martin-ueding.de>
2014-04-05 10:43:30 +0200

    Split action_step into two

--- Programm/System.cpp
+++ Programm/System.cpp
@@ -4,6 +4,7 @@
 #include "System.hpp"
 
 #include <cstddef>
+#include <iostream>
 
 System::System(double time_step, double mass) : time_step(time_step), mass(mass) {
 }
@@ -28,5 +29,7 @@ double System::action_difference(double prev, double cur, double alt,
 
 double System::action_step(double cur, double next) {
     double difference {next - cur};
-    return 0.5 * mass * (difference * difference) / (time_step * time_step) + potential(cur);
+    double kinetic_part {0.5 * mass * (difference * difference) / (time_step * time_step)};
+    double potential_part {potential(cur)};
+    return kinetic_part - potential_part;
 }
