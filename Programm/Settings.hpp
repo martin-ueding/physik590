9c5618447d72d16752ad1d853bd937bf3a75ed77
Martin Ueding <dev@martin-ueding.de>
2014-04-06 20:43:37 +0200

    More documentation

--- Programm/Settings.hpp
+++ Programm/Settings.hpp
@@ -16,25 +16,36 @@ class Settings {
     public:
         /**
           @f$ N @f$ in [CF80].
+
+          Number of division of the time lattice.
           */
         size_t time_sites {1000};
 
         /**
           @f$ M_0 @f$ in [CF80].
+
+          Mass of the particle.
           */
         double mass {1};
 
         /**
           @f$ a @f$ in [CF80].
+
+          Step size of the time lattice.
           */
         double time_step {0.1};
 
         /**
           @f$ \mu^2 @f$ in [CF80].
+
+          “Spring constant”.
           */
         double mu_squared {1};
 
-        double initial_random_width {0.63};
+        /**
+          Spread of positions in the random initialization.
+          */
+        double initial_random_width {0.632456};
 
         /**
           @f$ \Delta @f$ in [CF80].
@@ -45,27 +56,61 @@ class Settings {
           @f$ N_t @f$ in [CF80].
           */
         int pre_iterations {50};
+
+        /**
+          Just like \ref rounds, but for the equilibrium phase.
+          */
         int pre_rounds {5};
 
         /**
-          @f$ M @f$ in [CF80].
+          @f$ N_E @f$ in [CF80].
           */
         size_t iterations {10000};
-        int rounds {5};
 
         /**
           @f$ \bar n @f$ in [CF80].
           */
+        int rounds {5};
+
+        /**
+          Monte Carlo iterations between measurements.
+          */
         int iterations_between {2};
+
+        /**
+          Seed for the random generator that generates new positions.
+          */
         int position_seed {0};
+
+        /**
+          Seed for the random generator that is used to decide whether new
+          positions should be accepted.
+          */
         int accept_seed {0};
 
+        /**
+          Number of bins for the position histogram.
+          */
         size_t position_hist_bins {1000};
+
+        /**
+          Number of bins for the action histogram.
+          */
         size_t action_hist_bins {100};
 
+        /**
+          Generates a filename that contains the most important parameters.
+
+          @param[in] prefix Prefix.
+          @param[in] suffix Suffix.
+          @return Prefix-Parameters-Suffix.
+          */
         std::string generate_filename(std::string prefix, std::string suffix);
 
     private:
+        /**
+          Separator for parameters in the file name.
+          */
         const std::string separator {
             "-"
         };
