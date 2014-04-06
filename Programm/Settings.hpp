b340fcce445a61e8e717a892c9077d7be7327b41
Martin Ueding <dev@martin-ueding.de>
2014-04-06 20:33:40 +0200

    Document default values

--- Programm/Settings.hpp
+++ Programm/Settings.hpp
@@ -6,29 +6,66 @@
 
 #include <string>
 
+/**
+  Holds the parameters for a simulation.
+
+  The values listed here are the default values that are used unless they are
+  overwritten on the command line.
+  */
 class Settings {
     public:
-        size_t time_sites;
-        double mass;
-        double time_step;
-        double mu_squared;
+        /**
+          @f$ N @f$ in [CF80].
+          */
+        size_t time_sites {1000};
 
-        double initial_random_width;
-        double margin;
-        int pre_iterations;
-        int pre_rounds;
+        /**
+          @f$ M_0 @f$ in [CF80].
+          */
+        double mass {1};
 
-        size_t iterations;
-        int rounds;
-        int iterations_between;
-        int position_seed;
-        int accept_seed;
+        /**
+          @f$ a @f$ in [CF80].
+          */
+        double time_step {0.1};
 
-        size_t position_hist_bins;
-        size_t action_hist_bins;
+        /**
+          @f$ \mu^2 @f$ in [CF80].
+          */
+        double mu_squared {1};
+
+        double initial_random_width {0.63};
+
+        /**
+          @f$ \Delta @f$ in [CF80].
+          */
+        double margin {0.632456};
+
+        /**
+          @f$ N_t @f$ in [CF80].
+          */
+        int pre_iterations {50};
+        int pre_rounds {5};
+
+        /**
+          @f$ M @f$ in [CF80].
+          */
+        size_t iterations {10000};
+        int rounds {5};
+
+        /**
+          @f$ \bar n @f$ in [CF80].
+          */
+        int iterations_between {2};
+        int position_seed {0};
+        int accept_seed {0};
+
+        size_t position_hist_bins {1000};
+        size_t action_hist_bins {100};
 
         std::string generate_filename(std::string prefix, std::string suffix);
 
+    private:
         const std::string separator {
             "-"
         };
--- Programm/parse_arguments.cpp
+++ Programm/parse_arguments.cpp
@@ -15,36 +15,36 @@ bool parse_arguments(int argc, char **argv, Settings &settings) {
 
     boost::program_options::options_description oszillator_options {"Oscillator options"};
     oszillator_options.add_options()
-    ("time-bins,t", boost::program_options::value<size_t>(&settings.time_sites)->default_value(1000), "Number of sites in the time lattice")
-    ("mass,m", boost::program_options::value<double>(&settings.mass)->default_value(1), "Mass")
-    ("time-step,a", boost::program_options::value<double>(&settings.time_step)->default_value(0.1), "Spacing of time lattice")
-    ("mu-squared,o", boost::program_options::value<double>(&settings.mu_squared)->default_value(1), "μ²")
+    ("time-bins,t", boost::program_options::value<size_t>(&settings.time_sites)->default_value(settings.time_sites), "Number of sites in the time lattice")
+    ("mass,m", boost::program_options::value<double>(&settings.mass)->default_value(settings.mass), "Mass")
+    ("time-step,a", boost::program_options::value<double>(&settings.time_step)->default_value(settings.time_step), "Spacing of time lattice")
+    ("mu-squared,o", boost::program_options::value<double>(&settings.mu_squared)->default_value(settings.mu_squared), "μ²")
     ;
     options.add(oszillator_options);
 
     boost::program_options::options_description init_options {"Initialization options"};
     init_options.add_options()
-    ("irw", boost::program_options::value<double>(&settings.initial_random_width)->default_value(0.63), "Initial random width")
-    ("margin", boost::program_options::value<double>(&settings.margin)->default_value(0.632456), "Random margin, Δ")
-    ("pre-iterations", boost::program_options::value<int>(&settings.pre_iterations)->default_value(50), "Iterations to relax the system")
-    ("pre-rounds", boost::program_options::value<int>(&settings.pre_rounds)->default_value(5), "Rounds for a single x_j during relaxation")
+    ("irw", boost::program_options::value<double>(&settings.initial_random_width)->default_value(settings.initial_random_width), "Initial random width")
+    ("margin", boost::program_options::value<double>(&settings.margin)->default_value(settings.margin), "Random margin, Δ")
+    ("pre-iterations", boost::program_options::value<int>(&settings.pre_iterations)->default_value(settings.pre_iterations), "Iterations to relax the system")
+    ("pre-rounds", boost::program_options::value<int>(&settings.pre_rounds)->default_value(settings.pre_rounds), "Rounds for a single x_j during relaxation")
     ;
     options.add(init_options);
 
     boost::program_options::options_description iter_options {"Iteration options"};
     iter_options.add_options()
-    ("iterations,i", boost::program_options::value<size_t>(&settings.iterations)->default_value(10000), "Iterations for the histogram")
-    ("rounds,r", boost::program_options::value<int>(&settings.rounds)->default_value(5), "Rounds for a single x_j")
-    ("iterations-between", boost::program_options::value<int>(&settings.iterations_between)->default_value(2), "Extra iterations between measurements")
-    ("position-seed,s", boost::program_options::value<int>(&settings.position_seed)->default_value(0), "Seed for the random number generator used for positions")
-    ("accept-seed,c", boost::program_options::value<int>(&settings.accept_seed)->default_value(0), "Seed for the random number generator used for accepting new positions")
+    ("iterations,i", boost::program_options::value<size_t>(&settings.iterations)->default_value(settings.iterations), "Iterations for the histogram")
+    ("rounds,r", boost::program_options::value<int>(&settings.rounds)->default_value(settings.rounds), "Rounds for a single x_j")
+    ("iterations-between", boost::program_options::value<int>(&settings.iterations_between)->default_value(settings.iterations_between), "Extra iterations between measurements")
+    ("position-seed,s", boost::program_options::value<int>(&settings.position_seed)->default_value(settings.position_seed), "Seed for the random number generator used for positions")
+    ("accept-seed,c", boost::program_options::value<int>(&settings.accept_seed)->default_value(settings.accept_seed), "Seed for the random number generator used for accepting new positions")
     ;
     options.add(iter_options);
 
     boost::program_options::options_description hist_options {"Histogram options"};
     hist_options.add_options()
-    ("position-hist-bins,b", boost::program_options::value<size_t>(&settings.position_hist_bins)->default_value(1000), "Number of bins in the position histogram")
-    ("action-hist-bins", boost::program_options::value<size_t>(&settings.action_hist_bins)->default_value(100), "Number of bins in the action histogram")
+    ("position-hist-bins,b", boost::program_options::value<size_t>(&settings.position_hist_bins)->default_value(settings.position_hist_bins), "Number of bins in the position histogram")
+    ("action-hist-bins", boost::program_options::value<size_t>(&settings.action_hist_bins)->default_value(settings.action_hist_bins), "Number of bins in the action histogram")
     ;
     options.add(hist_options);
 
