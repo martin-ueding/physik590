315cbd601054aba38fc69c9cc08f83713ef76fa3
Martin Ueding <dev@martin-ueding.de>
2014-04-07 17:25:50 +0200

    New distribution for every single draw

--- Programm/MetropolisAlgorithm.cpp
+++ Programm/MetropolisAlgorithm.cpp
@@ -35,11 +35,11 @@ void MetropolisAlgorithm::iteration(int rounds, double margin) {
         unsigned int j_plus_one {Periodic::wrap(j + 1, x.list.size())};
         unsigned int j_minus_one {Periodic::wrap(j - 1, x.list.size())};
 
-        // Create a random number generator that will draw x_j' from the
-        // interval [x_j - margin, x_j + margin].
-        std::uniform_real_distribution<double> distribution {x.list[j] - margin, x.list[j] + margin};
-
         for (int round {0}; round < rounds; round++) {
+            // Create a random number generator that will draw x_j' from the
+            // interval [x_j - margin, x_j + margin].
+            std::uniform_real_distribution<double> distribution {x.list[j] - margin, x.list[j] + margin};
+
             // Generate a new random x_j'.
             double new_x {distribution(position_engine)};
 
