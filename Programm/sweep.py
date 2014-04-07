614691376336ad50cae324155305c7c4e50d6db1
Martin Ueding <dev@martin-ueding.de>
2014-04-07 17:13:01 +0200

    Sweep over iterations

--- Programm/sweep.py
+++ Programm/sweep.py
@@ -11,19 +11,20 @@ import subprocess
 def main():
     with concurrent.futures.ProcessPoolExecutor(4) as executor:
         futures = []
-        for a in [1, 2, 3]:
-            mods = ['-c', str(a)]
-            futures.append(executor.submit(run, mods[:]))
+        for X in [1, 2, 3]:
+            futures.append(executor.submit(run, ['-c', str(X)]))
 
-        for s in [1, 2, 3]:
-            mods = ['-s', str(s)]
-            futures.append(executor.submit(run, mods[:]))
+        for X in [1, 2, 3]:
+            futures.append(executor.submit(run, ['-s', str(X)]))
+
+        for X in [300, 1000, 3000]:
+            futures.append(executor.submit(run, ['-i', str(X), '-b', str(X // 3)]))
 
         for future in futures:
             future.result()
 
 def run(mods):
-    command = ['./metropolis', '-i', '300'] + mods
+    command = ['./metropolis'] + mods
     print(' '.join(command))
     output = subprocess.check_output(command).decode()
     return output
