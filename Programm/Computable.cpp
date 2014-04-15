c1cd3475d47b5a9f1dfd0259505604b76547b90e
Martin Ueding <dev@martin-ueding.de>
2014-04-15 12:43:47 +0200

    Add Computable stub

---
+++ Programm/Computable.cpp
@@ -0,0 +1,8 @@
+// Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+// Licensed under The GNU Public License Version 2 (or later)
+
+#include "Computable.hpp"
+
+std::string Computable::get_name() {
+    return "Computable";
+}
---
+++ Programm/Computable.hpp
@@ -0,0 +1,14 @@
+// Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+// Licensed under The GNU Public License Version 2 (or later)
+
+#pragma once
+
+#include "BootstrapSample.hpp"
+
+#include <string>
+
+class Computable {
+    public:
+        virtual void add_sample(BootstrapSample sample) = 0;
+        virtual std::string get_name();
+};
