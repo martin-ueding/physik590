2bf5878aa27e3341916b947924b61a2e805ad852
Martin Ueding <dev@martin-ueding.de>
2014-04-15 12:44:43 +0200

    Add ScalarComputable

---
+++ Programm/ScalarComputable.hpp
@@ -0,0 +1,7 @@
+// Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+// Licensed under The GNU Public License Version 2 (or later)
+
+#pragma once
+
+class ScalarComputable : public Computable, public BootstrappedQuantity {
+};
