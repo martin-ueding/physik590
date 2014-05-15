85576adc3aaf8135330177d63d4c90654a77e660
Martin Ueding <dev@martin-ueding.de>
2014-05-15 18:47:30 +0200

    Work on BootstrapPool

--- Programm/BootstrapPool.cpp
+++ Programm/BootstrapPool.cpp
@@ -5,8 +5,7 @@
 
 #include "ProgressBar.hpp"
 
-BootstrapPool::BootstrapPool(MetropolisDriver &driver, size_t iterations)
-    : iterations(iterations) {
+BootstrapPool::BootstrapPool(MetropolisDriver &driver, size_t iterations) {
     ProgressBar bar{"Populating bootstrap pool", iterations};
     for (size_t i {0}; i < iterations; ++i) {
         pool.push_back(driver.next());
--- Programm/BootstrapPool.hpp
+++ Programm/BootstrapPool.hpp
@@ -3,25 +3,43 @@
 
 #pragma once
 
-#include "ListQuantity.hpp"
-
 #include "MetropolisDriver.hpp"
 
 #include <vector>
 
+/**
+  The base set of trajectories that are used for bootstrapping later on.
+  */
 class BootstrapPool {
     public:
+        /**
+          Fills the pool with trajectories.
+
+          @param[in] driver Driver that generates trajectories
+          @param[in] iterations Number of pool entries
+          */
         BootstrapPool(MetropolisDriver &driver, size_t iterations);
 
+        /**
+          Gives the size of the pool
+          */
         size_t size() {
             return pool.size();
         }
 
+        /**
+          Gives access to the elements in the pool.
+          */
         ListQuantity &operator[](size_t i) {
             return pool[i];
         }
 
     protected:
-        std::vector<ListQuantity> pool;
-        size_t iterations;
+        /**
+          The pool itself.
+
+          Each entry in the primary pool is just a trajectory from the
+          Metropolis algorithm.
+          */
+        std::vector<std::vector<double>> pool;
 };
