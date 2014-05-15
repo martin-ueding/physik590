f87ca5b25ae2939dd133f1cf02c8a205a9d67877
Martin Ueding <dev@martin-ueding.de>
2014-05-15 13:28:58 +0200

    Calculate all possible correlations
    Before, only the correlations with x(0) and x(t) were calculated. Now
    this will calculate all correlations x(t) and x(t + Δt) for all possible
    t.
    
    I have the impression that some effects where caused of this.

--- Programm/Correlation.cpp
+++ Programm/Correlation.cpp
@@ -3,6 +3,8 @@
 
 #include "Correlation.hpp"
 
+#include "Periodic.hpp"
+
 #include <cassert>
 #include <sstream>
 
@@ -11,18 +13,25 @@ Correlation::Correlation(unsigned int distance) : distance {distance} {
 
 void Correlation::add_sample(BootstrapSample &sample) {
     size_t count {0};
-    double value_0 {0.0};
+    double value {0.0};
 
     for (size_t trajectory_id {0}; trajectory_id < sample.size(); trajectory_id++) {
-        ListQuantity &cur = sample[trajectory_id];
-
-        assert(distance + 1 < cur.list.size());
-
-        value_0 += cur.list[0] * cur.list[distance];
+        value += average_all(sample[trajectory_id].list, distance);
         count++;
     }
 
-    append(value_0 / count);
+    append(value / count);
+}
+
+double Correlation::average_all(std::vector<double> &list, const unsigned distance) {
+    double sum {0};
+    size_t max {list.size()};
+
+    for (unsigned i {0}; i < max; ++i) {
+        sum += list[i] * list[Periodic::wrap(i + distance, max)];
+    }
+
+    return sum / max;
 }
 
 std::string Correlation::get_name() {
--- Programm/Correlation.hpp
+++ Programm/Correlation.hpp
@@ -25,4 +25,16 @@ class Correlation : public ScalarComputable {
 
     protected:
         unsigned int distance;
+
+        /**
+          Averages all correlations with a given distance in that list.
+
+          Using periodic boundary conditions, there are as many correlations to
+          be extracted as there are elements in the given list. All those are
+          averaged.
+
+          @param[in] list List with data points
+          @param[in] distance Difference in steps
+          */
+        double average_all(std::vector<double> &list, const unsigned distance);
 };
