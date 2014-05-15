9094b3bc437a8a8e4aafb449f1b3a8dbcda8d6b0
Martin Ueding <dev@martin-ueding.de>
2014-05-15 18:03:31 +0200

    Remove other part

--- Programm/Trajectory.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
-// Licensed under The GNU Public License Version 2 (or later)
-
-#include "Trajectory.hpp"
-
-#include <iostream>
-
-Trajectory::Trajectory(ListQuantity l) : ListQuantity(l) {
-}
