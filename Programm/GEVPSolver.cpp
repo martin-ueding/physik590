7d358ee255bc07203cf916798a3ba8381b808d5d
Martin Ueding <dev@martin-ueding.de>
2014-05-16 13:48:07 +0200

    Deactivate output

--- Programm/GEVPSolver.cpp
+++ Programm/GEVPSolver.cpp
@@ -9,31 +9,15 @@
 #include <iostream>
 
 std::vector<double> GEVPSolver::eigenvalues(Eigen::MatrixXd &a, Eigen::MatrixXd &b) {
-    std::cout << "A" << std::endl;
-    std::cout << a << std::endl;
-    std::cout << "B" << std::endl;
-    std::cout << b << std::endl;
     Eigen::MatrixXd l = b.llt().matrixL();
     Eigen::MatrixXd l_dagger = l.adjoint();
     Eigen::MatrixXd l_inverse = l.inverse();
     Eigen::MatrixXd l_dagger_inverse = l_dagger.inverse();
 
-    std::cout << "L" << std::endl;
-    std::cout << l << std::endl;
-    std::cout << "L dagger" << std::endl;
-    std::cout << l_dagger << std::endl;
-    std::cout << "L dagger inverse" << std::endl;
-    std::cout << l_dagger_inverse << std::endl;
-    std::cout << "L inverse" << std::endl;
-    std::cout << l_inverse << std::endl;
-
     Eigen::MatrixXd a_prime = l_inverse * a * l_dagger_inverse;
 
     Eigen::EigenSolver<Eigen::MatrixXd> ev_solver {a_prime, false};
     auto eigenvalues = ev_solver.eigenvalues();
-
-    std::cout << "Eigenvalues" << std::endl;
-    std::cout << eigenvalues << std::endl;
 
     std::vector<double> eigenvalues_vector;
 
@@ -43,6 +27,24 @@ std::vector<double> GEVPSolver::eigenvalues(Eigen::MatrixXd &a, Eigen::MatrixXd 
     }
 
     sort(eigenvalues_vector.begin(), eigenvalues_vector.end());
+
+    /*
+    std::cout << "A" << std::endl;
+    std::cout << a << std::endl;
+    std::cout << "B" << std::endl;
+    std::cout << b << std::endl;
+    std::cout << "L" << std::endl;
+    std::cout << l << std::endl;
+    std::cout << "L dagger" << std::endl;
+    std::cout << l_dagger << std::endl;
+    std::cout << "L dagger inverse" << std::endl;
+    std::cout << l_dagger_inverse << std::endl;
+    std::cout << "L inverse" << std::endl;
+    std::cout << l_inverse << std::endl;
+    std::cout << "Eigenvalues" << std::endl;
+    std::cout << eigenvalues << std::endl;
+
+    */
 
     return eigenvalues_vector;
 }
