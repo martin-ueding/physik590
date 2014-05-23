5a8b83a3c9ac9d53370a531a00fe2fecfd513a36
Martin Ueding <dev@martin-ueding.de>
2014-05-23 13:10:10 +0200

    Restructure options

--- Programm/parse_arguments.cpp
+++ Programm/parse_arguments.cpp
@@ -8,104 +8,97 @@
 #include <iostream>
 
 void parse_arguments(int argc, char **argv, Settings &settings) {
-    boost::program_options::options_description options {"Program options"};
+    boost::program_options::options_description options {"Program"};
     options.add_options()
     ("help,h", "Print usage and exit")
     ;
 
-    boost::program_options::options_description oszillator_options {
-        "Oscillator options"
+    boost::program_options::options_description group1 {
+        "Simulation"
     };
-    oszillator_options.add_options()
-    ("time-bins,t",
+    group1.add_options()
+    ("time-sites",
      boost::program_options::value<unsigned>(&settings.time_sites)->default_value(settings.time_sites),
      "Number of sites in the time lattice")
-    ("mass,m",
+    ("mass",
      boost::program_options::value<double>(&settings.mass)->default_value(settings.mass),
      "Mass")
-    ("time-step,a",
+    ("time-step",
      boost::program_options::value<double>(&settings.time_step)->default_value(settings.time_step),
      "Spacing of time lattice")
-    ("mu-squared,o",
+    ("mu",
      boost::program_options::value<double>(&settings.mu_squared)->default_value(settings.mu_squared),
      "μ²")
-    ("inverse-scattering-length",
+    ("inv-scatt-len",
      boost::program_options::value<double>(&settings.inverse_scattering_length)->default_value(settings.inverse_scattering_length),
      "Gauss height")
     ("gauss-width",
      boost::program_options::value<double>(&settings.gauss_width)->default_value(settings.gauss_width),
      "Gauss width")
-    ;
-    options.add(oszillator_options);
-
-    boost::program_options::options_description init_options {
-        "Initialization options"
-    };
-    init_options.add_options()
     ("irw",
      boost::program_options::value<double>(&settings.initial_random_width)->default_value(settings.initial_random_width),
      "Initial random width")
     ("margin",
      boost::program_options::value<double>(&settings.margin)->default_value(settings.margin),
      "Random margin, Δ")
-    ("pre-iterations",
+    ("pre-iter",
      boost::program_options::value<unsigned>(&settings.pre_iterations)->default_value(settings.pre_iterations),
      "Iterations to relax the system")
     ("pre-rounds",
      boost::program_options::value<unsigned>(&settings.pre_rounds)->default_value(settings.pre_rounds),
      "Rounds for a single x_j during relaxation")
-    ;
-    options.add(init_options);
-
-    boost::program_options::options_description iter_options {
-        "Iteration options"
-    };
-    iter_options.add_options()
-    ("iterations,i",
+    ("iter",
      boost::program_options::value<unsigned>(&settings.iterations)->default_value(settings.iterations),
      "Iterations for the histogram")
-    ("rounds,r",
+    ("rounds",
      boost::program_options::value<unsigned>(&settings.rounds)->default_value(settings.rounds),
      "Rounds for a single x_j")
-    ("iterations-between",
+    ("iter-between",
      boost::program_options::value<unsigned>(&settings.iterations_between)->default_value(settings.iterations_between),
      "Extra iterations between measurements")
-    ("position-seed,s",
+    ("position-seed",
      boost::program_options::value<int>(&settings.position_seed)->default_value(settings.position_seed),
      "Seed for the random number generator used for positions")
-    ("accept-seed,c",
+    ("accept-seed",
      boost::program_options::value<int>(&settings.accept_seed)->default_value(settings.accept_seed),
      "Seed for the random number generator used for accepting new positions")
-    ("samples",
-     boost::program_options::value<unsigned>(&settings.bootstrap_samples)->default_value(settings.bootstrap_samples),
-     "Number of bootstrap samples to generate")
-    ;
-    options.add(iter_options);
-
-    boost::program_options::options_description hist_options {
-        "Histogram options"
-    };
-    hist_options.add_options()
-    ("position-hist-bins,b",
+    ("pos-hist-bins",
      boost::program_options::value<unsigned>(&settings.position_hist_bins)->default_value(settings.position_hist_bins),
      "Number of bins in the position histogram")
     ("action-hist-bins",
      boost::program_options::value<unsigned>(&settings.action_hist_bins)->default_value(settings.action_hist_bins),
      "Number of bins in the action histogram")
-    ("correlation-size",
+    ("corr-size",
      boost::program_options::value<unsigned>(&settings.correlation_size)->default_value(settings.correlation_size),
      "Size of the correlation matrix. Maximum eigenvalue is twice this.")
     ("corr-tau-max",
      boost::program_options::value<double>(&settings.corr_tau_max)->default_value(settings.corr_tau_max),
      "Last τ to calculate C(τ) for.")
+    ;
+    options.add(group1);
+
+    boost::program_options::options_description group3 {
+        "Analysis"
+    };
+    group3.add_options()
     ("load",
      boost::program_options::value<std::string>(&settings.load_filename)->default_value(""),
      "Load this file")
-    ("max-cores,j",
+    ("samples",
+     boost::program_options::value<unsigned>(&settings.bootstrap_samples)->default_value(settings.bootstrap_samples),
+     "Number of bootstrap samples to generate")
+    ;
+    options.add(group3);
+
+    boost::program_options::options_description group2 {
+        "Common"
+    };
+    group2.add_options()
+    ("max-cores",
      boost::program_options::value<unsigned>(&settings.max_cores)->default_value(settings.max_cores),
      "Maximum number of parallel threads.")
     ;
-    options.add(hist_options);
+    options.add(group2);
 
     boost::program_options::variables_map vm;
     boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), vm);
