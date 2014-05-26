78dfc0b9e8549e888bb1b665e87124f0c5b35a41
Martin Ueding <dev@martin-ueding.de>
2014-05-26 19:52:01 +0200

    Test Settings::matrix_to_state

--- Programm/SettingsTest.cpp
+++ Programm/SettingsTest.cpp
@@ -6,5 +6,15 @@
 #include "Settings.hpp"
 
 void SettingsTest::test_numbers() {
-    CPPUNIT_ASSERT(true);
+    Settings settings;
+
+    CPPUNIT_ASSERT(settings.matrix_to_state(0, true) == 2);
+    CPPUNIT_ASSERT(settings.matrix_to_state(1, true) == 4);
+    CPPUNIT_ASSERT(settings.matrix_to_state(0, false) == 1);
+    CPPUNIT_ASSERT(settings.matrix_to_state(1, false) == 3);
+
+    CPPUNIT_ASSERT(settings.state_to_matrix(2) == 0);
+    CPPUNIT_ASSERT(settings.state_to_matrix(4) == 1);
+    CPPUNIT_ASSERT(settings.state_to_matrix(1) == 0);
+    CPPUNIT_ASSERT(settings.state_to_matrix(3) == 1);
 }
