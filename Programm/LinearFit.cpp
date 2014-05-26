4657b9321e63c57839d8a4414bc684ecafeda78d
Martin Ueding <dev@martin-ueding.de>
2014-05-26 16:50:45 +0200

    Throw exception

--- Programm/LinearFit.cpp
+++ Programm/LinearFit.cpp
@@ -5,16 +5,13 @@
 
 #include <gsl/gsl_fit.h>
 
+#include <stdexcept>
+
 LinearFit::LinearFit(std::vector<double> x, std::vector<double> y,
                       std::vector<double> y_err) {
-    /*
-    int gsl_fit_wlinear(const double *x, const size_t xstride, const double *w,
-                        const size_t wstride, const double *y,
-                        const size_t ystride, size_t n, double *c0, double *c1,
-                        double *cov00, double *cov01, double * cov11,
-                        double *chisq)
-    */
-    // TODO Assert that x, y and y_err have the same size.
+    if (x.size() != y.size() || y.size() != y_err.size()) {
+        throw std::range_error("All input has to have same size.");
+    }
 
     // Invert errors to give weights.
     std::vector<double> weights(y_err.size());
@@ -22,6 +19,13 @@ LinearFit::LinearFit(std::vector<double> x, std::vector<double> y,
         weights[i] = 1 / y_err[i];
     }
 
+    /*
+    int gsl_fit_wlinear(const double *x, const size_t xstride, const double *w,
+                        const size_t wstride, const double *y,
+                        const size_t ystride, size_t n, double *c0, double *c1,
+                        double *cov00, double *cov01, double * cov11,
+                        double *chisq)
+    */
     gsl_fit_wlinear(x.data(), 1, weights.data(), 1, y.data(), 1, x.size(),
                     &c0, &c1, &cov00, &cov01, &cov11, &chisq);
 }
