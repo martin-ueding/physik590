6ac1e1e244cdc45186589ef4dd874e1f3155d00e
Martin Ueding <dev@martin-ueding.de>
2014-05-26 19:44:09 +0200

    Revert "Remove test driver"
    This reverts commit 372a6923b7d4771f9dfd61cd4e18b65a426144f6.

---
+++ Programm/main-metropolis-test.cpp
@@ -0,0 +1,22 @@
+// Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+// Licensed under The GNU Public License Version 2 (or later)
+
+/**
+  @file
+  */
+
+#include <cppunit/TextTestRunner.h>
+
+#include "HarmonicOscillatorTest.hpp"
+#include "MetropolisAlgorithmTest.hpp"
+#include "PeriodicTest.hpp"
+
+int main() {
+    CppUnit::TextTestRunner runner;
+    runner.addTest(PeriodicTest::suite());
+    runner.addTest(MetropolisAlgorithmTest::suite());
+    runner.addTest(HarmonicOscillatorTest::suite());
+    runner.run();
+    return 0;
+}
+
