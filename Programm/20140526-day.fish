9ac313a6745dd5bdb90063956c043ba3c01c114a
Martin Ueding <dev@martin-ueding.de>
2014-05-27 16:15:28 +0200

    Add another job script

---
+++ Programm/20140526-day.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/fish
+# Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+# Licensed under The GNU Public License Version 2 (or later)
+
+# This file is from 2014-05-26. It will run over night and compute data points for a few different values of the inverse scattering length.
+
+cd _build/Release
+
+make -j4
+
+set common --iter 20000 --samples 10000 --corr-tau-max 6 --corr-size 6
+
+time nice ./metropolis $common --inv-scatt-len -1 --gauss-width 0.1
+time nice ./metropolis $common --inv-scatt-len -1 --gauss-width 0.01
+
+time nice ./metropolis $common --inv-scatt-len 1 --gauss-width 0.3
+time nice ./metropolis $common --inv-scatt-len 1 --gauss-width 0.8
+time nice ./metropolis $common --inv-scatt-len 1 --gauss-width 0.001
