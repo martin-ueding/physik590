6743b160e259df8593306f82abd8e34386319daa
Martin Ueding <dev@martin-ueding.de>
2014-06-08 18:32:21 +0200

    Write run numbers into file

--- Programm/20140608-run.fish
+++ Programm/20140608-run.fish
@@ -8,5 +8,5 @@ function common
     nice time ./metropolis --iter 25000 --time-sites 30000 --corr-size 5 --corr-tau-count 20 $argv
 end
 
-common --time-step 0.05 --corr-tau-max 4
-common --time-step 0.005 --corr-tau-max 4
+common --time-step 0.05 --corr-tau-max 4 # A989F7
+common --time-step 0.005 --corr-tau-max 4 # B006EB
