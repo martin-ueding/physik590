04af471fcc76c7c93c183becd32473cf51294d2b
Martin Ueding <dev@martin-ueding.de>
2014-06-08 13:36:20 +0200

    Calculate powers eagerly

--- Programm/Correlation.cpp
+++ Programm/Correlation.cpp
@@ -14,17 +14,35 @@ Eigen::MatrixXd correlation(std::vector<double> &x, Settings &settings,
     Eigen::MatrixXd c {settings.correlation_size, settings.correlation_size};
     c.setZero();
 
+    // Compute the powers of the trajectory so that the power function does not
+    // need to be invoked that often. I have not tested, but I assume that this
+    // is a hotspot.
+
+    std::vector<std::vector<double>> powers(c.rows());
+
     for (unsigned row {0u}; row < c.rows(); row++) {
-        unsigned power1 {settings.matrix_to_state(row, even)};
-        assert(power1 > 0);
+        unsigned power {settings.matrix_to_state(row, even)};
+        assert(power > 0);
+
+        // Bring the memory to the required size.
+        powers[row].resize(x.size());
+
+        // Take the power from all the x[k].
+        for (unsigned k {0u}; k < x.size(); ++k) {
+            powers[row][k] = std::pow(x[k], power);
+        }
+    }
+
+    for (unsigned row {0u}; row < c.rows(); row++) {
         for (unsigned col {0u}; col < c.cols(); col++) {
-            unsigned power2 {settings.matrix_to_state(col, even)};
-            assert(power2 > 0);
-            assert((power1 + power2) % 2 == 0);
             for (unsigned k {0u}; k < x.size(); ++k) {
-                double x1 {x[k]};
-                double x2 {x[Periodic::wrap(k + distance, x.size())]};
-                c(row, col) += std::pow(x1, power1) * std::pow(x2, power2);
+                // Calculate the needed k's.
+                unsigned k1 {k};
+                unsigned k2 {Periodic::wrap(k + distance, x.size())};
+
+                // Add x[k1]**power1 + x[k2]**power2 to the matrix element
+                // using the precomputed powers.
+                c(row, col) += powers[row][k1] * powers[col][k2];
             }
         }
     }
