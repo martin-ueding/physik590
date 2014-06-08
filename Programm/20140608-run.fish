f8df623dc4d866d1a4f37a78539b8046ed2eb4a3
Martin Ueding <dev@martin-ueding.de>
2014-06-08 13:15:15 +0200

    Add another run

---
+++ Programm/20140608-run.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/fish
+# Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+
+cd _build/Release
+make -j4
+
+function common
+    nice time ./metropolis --iter 25000 --time-sites 30000 --corr-size 5 --corr-tau-count 20 $argv
+end
+
+common --time-step 0.05 --corr-tau-max 4
+common --time-step 0.005 --corr-tau-max 4
