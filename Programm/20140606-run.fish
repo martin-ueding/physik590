7557c121d30ab9731c754d41c97284d8ce2c42be
Martin Ueding <dev@martin-ueding.de>
2014-06-08 13:02:59 +0200

    Add large amount of time-sites

--- Programm/20140606-run.fish
+++ Programm/20140606-run.fish
@@ -5,7 +5,7 @@ cd _build/Release
 make -j4
 
 function common
-    nice time ./metropolis --iter 30000 --inv-scatt-len 1 --gauss-width 0.055556 --corr-size 5 --corr-tau-count 30 $argv
+    nice time ./metropolis --iter 17000 --time-sites 30000 --inv-scatt-len 1 --gauss-width 0.055556 --corr-size 5 --corr-tau-count 20 $argv
 end
 
 common --time-step 0.05 --corr-tau-max 4
