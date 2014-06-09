0aacc24a0fdce3e48728eeb8eb2d3acbd3393f4b
Martin Ueding <dev@martin-ueding.de>
2014-06-09 14:44:58 +0200

    More runs

--- Programm/20140608-run.fish
+++ Programm/20140608-run.fish
@@ -5,8 +5,14 @@ cd _build/Release
 make -j4
 
 function common
-    nice time ./metropolis --iter 25000 --time-sites 30000 --corr-size 5 --corr-tau-count 20 $argv
+    nice time ./metropolis --iter 20000 --time-sites 30000 --corr-size 5 --corr-tau-count 20 $argv
 end
 
-common --time-step 0.05 --corr-tau-max 4 # A989F7
-common --time-step 0.005 --corr-tau-max 4 # B006EB
+#common --time-step 0.05 --corr-tau-max 4 # A989F7
+#common --time-step 0.005 --corr-tau-max 4 # B006EB
+
+common --time-step 0.05 --corr-tau-max 4 --inv-scatt-len 1 --gauss-width 0.55556
+common --time-step 0.005 --corr-tau-max 4 --inv-scatt-len 1 --gauss-width 0.55556
+
+common --time-step 0.05 --corr-tau-max 4 --inv-scatt-len -1 --gauss-width 0.055556
+common --time-step 0.005 --corr-tau-max 4 --inv-scatt-len -1 --gauss-width 0.055556
