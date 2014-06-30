57f3ebe12b861c932c2975e21407695bcb7ba24e
Martin Ueding <dev@martin-ueding.de>
2014-06-30 13:03:28 +0200

    Make Histograms allocatable

--- Programm/Histogram.hpp
+++ Programm/Histogram.hpp
@@ -11,6 +11,8 @@ class Histogram {
         friend class boost::serialization::access;
 
     public:
+        Histogram();
+
         Histogram(double min, double max, size_t bins);
 
         Histogram &operator+=(Histogram &other);
@@ -53,8 +55,6 @@ class Histogram {
 #pragma clang diagnostic pop
 
     protected:
-        Histogram();
-
         double min;
         double max;
         std::vector<int> bins;
